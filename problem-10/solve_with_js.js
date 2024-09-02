c40f5d90f7f3419e02dd7fff77ad8ed6d8db03b3
arasruislam <arasru01@gmail.com>
2024-09-03 04:17:11 +0600

    problem 10 solved

---
+++ problem-10/solve_with_js.js
@@ -0,0 +1,10 @@
+const MaxNumber = (a, b) => {
+   if (a < b) {
+      console.log(a, " is minimum number");
+   } else {
+      console.log(b, " is minimum number");
+   }
+};
+
+
+MaxNumber(5, 8)
---
+++ problem-10/solve_with_python.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+if a < b:
+    print(a, " is minimum number")
+else:
+    print(b, " is minimum number")
